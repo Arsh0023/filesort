06c2d6eaa006fa1be02e1af810ff324b416dedc2
Arshpreet Singh <arsharsh2391@gmail.com>
2020-02-09 23:41:16 +0530

    can sort .exe , .jpg , .msi , .pdf , .mp4

--- filesort.py
+++ filesort.py
@@ -5,7 +5,16 @@ import os,re,shutil
 os.chdir(r'D:\Chrome downloads') #enter the directory in which you want to perform the sort.
 files = os.listdir(r'D:\Chrome downloads')
 for i in files:
-	if(i.endswith(".exe")):
-		shutil.move(r'D:\Chrome Sorted\Setups')
+	if(i.endswith(".exe")|i.endswith(".msi")):
+		shutil.move(i,r'D:\Chrome Sorted\Setups')
 	if(i.endswith(".zip")):
-		shutil.move(r'D:\Chrome Sorted\Zip')
+		shutil.move(i,r'D:\Chrome Sorted\Zip')
+	if(i.endswith(".pdf")):
+		shutil.move(i,r'D:\Chrome Sorted\PDFs')
+	if(i.endswith(".rar")):
+		shutil.move(i,r'D:\Chrome Sorted\rar_files')
+	if(i.endswith(".mp4")):
+		shutil.move(i,r'D:\Chrome Sorted\videos')
+	if(i.endswith(".jpg")):
+		shutil.move(i,r'D:\Chrome Sorted\Images')
+print("All files are placed in their respective folder")
