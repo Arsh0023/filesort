e751dff1061dc54e2eb41ad29359d4f85382d16b
Arshpreet Singh <arsharsh2391@gmail.com>
2020-02-10 00:01:26 +0530

    Now calculates number of files and files moved

--- filesort.py
+++ filesort.py
@@ -4,17 +4,28 @@
 import os,re,shutil
 os.chdir(r'D:\Chrome downloads') #enter the directory in which you want to perform the sort.
 files = os.listdir(r'D:\Chrome downloads')
+n = 0
+total = 0
 for i in files:
 	if(i.endswith(".exe")|i.endswith(".msi")):
 		shutil.move(i,r'D:\Chrome Sorted\Setups')
+		total+=1
 	if(i.endswith(".zip")):
 		shutil.move(i,r'D:\Chrome Sorted\Zip')
+		total+=1
 	if(i.endswith(".pdf")):
 		shutil.move(i,r'D:\Chrome Sorted\PDFs')
+		total+=1
 	if(i.endswith(".rar")):
 		shutil.move(i,r'D:\Chrome Sorted\rar_files')
+		total+=1
 	if(i.endswith(".mp4")):
 		shutil.move(i,r'D:\Chrome Sorted\videos')
+		total+=1
 	if(i.endswith(".jpg")):
 		shutil.move(i,r'D:\Chrome Sorted\Images')
+		total+=1
+	n+=1
+print("total files - ",n)
+print("number of files moved - ",total)
 print("All files are placed in their respective folder")
