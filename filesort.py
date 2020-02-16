48ff9438d5e413573c7bcc814cb4500637018011
Arshpreet Singh <arsharsh2391@gmail.com>
2020-02-16 15:46:44 +0530

    now shows the name of the files moved

--- filesort.py
+++ filesort.py
@@ -3,6 +3,7 @@
 #sorts the files based on their type and then move them into the catogerized folders
 import os,re,shutil
 q = 0
+moved_files = [] # making support for showing the name of the files moved 
 path = os.path.isdir(r'D:\Chrome Downloads') #boolean for path
 if(path):
 	if(len(os.listdir(r'D:\Chrome Downloads')) != 0):                                                            #files should be there in directory
@@ -29,22 +30,29 @@ if(path):
 		for i in files:
 			if(i.endswith(".exe")|i.endswith(".msi")):
 				shutil.move(i,r'D:\Chrome Sorted\Setups')
+				moved_files.append(i)
 				total+=1
 			if(i.endswith(".zip")):
 				shutil.move(i,r'D:\Chrome Sorted\Zip')
+				moved_files.append(i)
 				total+=1
 			if(i.endswith(".pdf")):
 				shutil.move(i,r'D:\Chrome Sorted\PDFs')
+				moved_files.append(i)
 				total+=1
 			if(i.endswith(".rar")):
 				shutil.move(i,r'D:\Chrome Sorted\rar_files')
 				total+=1
+				moved_files.append(i)
 			if(i.endswith(".mp4")):
 				shutil.move(i,r'D:\Chrome Sorted\videos')
 				total+=1
+				moved_files.append(i)
 			if(i.endswith(".jpg")):
 				shutil.move(i,r'D:\Chrome Sorted\Images')
 				total+=1
+				moved_files.append(i)
+
 			n+=1
 		if(q != 0):
 			print("\n\n")
@@ -53,6 +61,12 @@ if(path):
 		print("files left - ",n-total)
 		if(total != 0):
 			print("All files are placed in their respective folder")
+			count = 1
+			print("\n\nfiles moved - "+"\n")
+			for i in moved_files:
+				print(str(count)+". ",i)
+				count+=1
+
 	else:
 		print("Place the files you want to sort in D:\\Chrome Downloads and run the program again")
 else:
